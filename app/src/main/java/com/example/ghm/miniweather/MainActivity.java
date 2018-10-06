99126fe6c18c348cd83f2d4961f571186d3deea9
glucin <ghm_1996@163.com>
2018-10-06 21:32:36 +0800

    second commit

---
+++ app/src/main/java/com/example/ghm/miniweather/MainActivity.java
@@ -0,0 +1,11 @@
+package com.example.ghm.miniweather;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity{
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.weather_info);
+    }
+}
